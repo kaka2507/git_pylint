2aa20e60f3d8d34fc63a278ddfb0d0cc2e08d88c
Bao Doan N <doanngocbao@gmail.com>
2020-12-13 09:43:52 +0700

    Upd: support to specify target branch

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Support to run Pylint on python projects
 ```shell script
 - name: Python Pylin GitHub Action
   uses: vcoder4c/git_pylint@v1
+  env:
+    DEFAULT_TARGET_BRANCH: master
 ```
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-pylint_git --ignore-patterns=".*_pb2\.py",".*template\.py","(.*_)?test(_.*)?\.py","(.*_)?mock.*\.py","^\d{4}_.*\.py" --indent-string="    " --disable=C0122 --output-format=colorized --reports=no --msg-template="{abspath} ({line}:{column}) {obj} [{msg_id}:{symbol}] {msg}"
+pylint_git --ignore-patterns=".*_pb2\.py",".*template\.py","(.*_)?test(_.*)?\.py","(.*_)?mock.*\.py","^\d{4}_.*\.py" --indent-string="    " --disable=C0122 --output-format=colorized --reports=no --msg-template="{abspath} ({line}:{column}) {obj} [{msg_id}:{symbol}] {msg}" --target-branch=$env:DEFAULT_TARGET_BRANCH
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
