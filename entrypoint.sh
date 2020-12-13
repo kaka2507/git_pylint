2f92f71a28f509edae260a176b4b89030c2f3fcc
Bao Doan N <doanngocbao@gmail.com>
2020-12-13 09:55:28 +0700

    Fix: correct the way to use envs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,7 @@ set -e
 
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
-echo ${DEFAULT_TARGET_BRANCH}
-echo $env
 
-pylint_git --ignore-patterns=".*_pb2\.py",".*template\.py","(.*_)?test(_.*)?\.py","(.*_)?mock.*\.py","^\d{4}_.*\.py" --indent-string="    " --disable=C0122 --output-format=colorized --reports=no --msg-template="{abspath} ({line}:{column}) {obj} [{msg_id}:{symbol}] {msg}" --target-branch=$env:DEFAULT_TARGET_BRANCH
-
+pylint_git --ignore-patterns=".*_pb2\.py",".*template\.py","(.*_)?test(_.*)?\.py","(.*_)?mock.*\.py","^\d{4}_.*\.py" --indent-string="    " --disable=C0122 --output-format=colorized --reports=no --msg-template="{abspath} ({line}:{column}) {obj} [{msg_id}:{symbol}] {msg}" --target-branch=${DEFAULT_TARGET_BRANCH}
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
