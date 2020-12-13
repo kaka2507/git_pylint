99d36a68d9265e8ff12685fb81d1d1643ee8362d
Bao Doan N <doanngocbao@gmail.com>
2020-12-13 09:50:17 +0700

    Fix: debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
+echo ${DEFAULT_TARGET_BRANCH}
+echo $env
 
 pylint_git --ignore-patterns=".*_pb2\.py",".*template\.py","(.*_)?test(_.*)?\.py","(.*_)?mock.*\.py","^\d{4}_.*\.py" --indent-string="    " --disable=C0122 --output-format=colorized --reports=no --msg-template="{abspath} ({line}:{column}) {obj} [{msg_id}:{symbol}] {msg}" --target-branch=$env:DEFAULT_TARGET_BRANCH
 
